c1fcb486f5ef1a236c6204dd28711da6e27d6f71
Polyvios Papakonstantinou <polpap@hit.com.gr>
2025-06-12 16:37:38 +0300

    further ui implementation

--- src/app/main-view/days-view/days-view.component.ts
+++ src/app/main-view/days-view/days-view.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-days-view',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './days-view.component.html',
   styleUrl: './days-view.component.css'
 })
 export class DaysViewComponent {
+  wholeDayHours: string[] = [];
+  dayTimeHours: string[] = [];
 
+  constructor() {
+    this.generateHours();
+  }
+
+  generateHours() {
+    for (let i = 0; i < 24; i++) {
+      this.wholeDayHours.push(i.toString().padStart(2, '0'));
+    }
+
+    for (let i = 8; i < 21; i++) {
+      this.dayTimeHours.push(i.toString().padStart(2, '0'));
+    }
+  }
 }
